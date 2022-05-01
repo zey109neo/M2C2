5f745db4ef59ef20082006aefdd061086fb31ed5
zey109neo <34295499+zey109neo@users.noreply.github.com>
2022-04-30 23:03:23 -0500

    Add files via upload

---
+++ M2C2.py
@@ -0,0 +1,25 @@
+# Exercise 1
+sentence = "the cow jumped over the moon"
+number = 5
+students = [1,2,3]
+went_for_walk = True
+# Exercise 2
+first_word = sentence[0:3]
+# Exercise 3
+first_element = sentence[0]
+# Exercise 4
+newnum = number + 10
+# Exercise 5
+lastnum = students[2]
+# exercise 6
+names = 'harry,alex,susie,jared,gail,conner'
+li = list(names.split(","))
+# exercise 7
+sentencelist = sentence.split()
+second_part_of_sentence = ' '.join(sentencelist[1:])
+first_word = (first_word.upper())
+finishedsentence = first_word + " " + second_part_of_sentence
+# exercise 8 
+print(f'There are {number} students')
+# exercise 9
+print('hello world')
